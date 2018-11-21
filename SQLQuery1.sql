6198aeaf66bd5485456c924121ba66a009bc1b6b
cph-ml444 <31694580+cph-ml444@users.noreply.github.com>
2018-11-21 02:16:45 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[sp_insert]  
+(  
+    -- Add the parameters for the stored procedure here  
+  
+      
+    @phone varchar(50),  
+    @password varchar(50),  
+    @email varchar(50)  
+  
+)  
+as  
+Begin  
+  
+    Declare @Count int  
+    Declare @codereturn int  
+      
+    Select @Count = COUNT(email)  
+    from tbl_data where email = @email 
+    If @Count > 0  
+    Begin   
+      
+        Set @codereturn = -1  
+    End  
+    Else  
+    Begin  
+          
+        Set @codereturn  = 1  
+        Insert into tbl_data values(@phone,@password,@email)  
+      
+    End  
+    Select @codereturn as ReturnValue  
+      
+End   
---
+++ SQLQuery2.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE sp_select  
+      
+    @email varchar(50),  
+    @password varchar(50)  
+      
+AS  
+BEGIN  
+    Declare @Count int  
+      
+    Select @Count = COUNT(email)  
+    from tbl_data where [email] =@email and [password] =@password 
+      
+    If (@Count = 1)  
+    Begin   
+     Select 1 as codereturn  
+    End  
+    Else  
+    Begin  
+        Select -1 as codereturn  
+    End  
+      
+      
+END   
